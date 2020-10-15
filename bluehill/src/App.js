f2067d79e9489d241707f8b91449a674a895d53a
Samuel Pike <samuelpike1@Samuels-iMac.local>
2020-10-15 13:10:55 -0400

    first commit

---
+++ bluehill/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import About from './components/About';
+
+const items = {
+  {
+    title: "Our Story",
+    content: "lorem ipsum"
+  },
+  {
+    title: "Our Meats"
+    content: "Grassfed and full of flavor"
+  },
+  {
+    title: "Our Location",
+    content: "655 Blue Hill Ave, Boston MA"
+  }
+};
+
+const App =  () => {
+  const []
+  return (
+    <h1>
+      <About />
+  </h1>
+);
+};
+
+export default App;
---
+++ bluehill/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="ui form active">
+        <div className="ui field">
+          <label>Butcher info</label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
